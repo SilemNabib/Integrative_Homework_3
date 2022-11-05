f93a971c792d6bf60ccb7c14ad424bbe4ea23f77
Silem <silemnabib@gmail.com>
2022-11-04 23:00:18 -0500

    1° Requerimiento

--- src/ui/NeoTunesManager.java
+++ src/ui/NeoTunesManager.java
@@ -1,9 +1,13 @@
 package ui;
 
+import java.util.Scanner;
+
 import model.NeoTunesController;
 
 public class NeoTunesManager {
     NeoTunesController neotunesObj;
+
+    public Scanner sc = new Scanner(System.in);
 
     public NeoTunesManager() {
          neotunesObj = new NeoTunesController();
@@ -11,7 +15,6 @@ public class NeoTunesManager {
 
     public static void main(String[] args){
         NeoTunesManager system = new NeoTunesManager();
-
     }
 
     public void menu(){
@@ -19,6 +22,59 @@ public class NeoTunesManager {
     }
 
     public void createUser(){
+        String nickname;
+        String id;
+        int option;
         
+        System.out.println("nickName: ");
+        nickname = sc.nextLine();
+
+        System.out.println("Id: ");
+        id = sc.nextLine();
+
+        if (neotunesObj.checkIfIdExists(id)){
+            
+            System.out.println("Consumidor o Productor (1 o 2): ");
+            option = sc.nextInt();
+            sc.nextLine();
+
+            switch(option) {
+                case 1:
+                    int consumerType;
+
+                    System.out.println("Estandar o premium (1 o 2): ");
+                    consumerType = sc.nextInt();
+                    sc.nextLine();
+
+                    neotunesObj.createConsumer(consumerType, nickname, id);
+                    
+                    break;
+
+                case 2:
+                    int producerType;
+                    String name;
+                    String url;
+
+                    System.out.println("Nombre: ");
+                    name = sc.nextLine();
+
+                    System.out.println("Url: ");
+                    url = sc.nextLine();
+
+                    System.out.println("Artista o creador de contenido (1 o 2): ");
+                    producerType = sc.nextInt();
+                    sc.nextLine();
+
+                    neotunesObj.createProducer(producerType, nickname, id, name, url);
+                    break;
+            }
+            
+
+        } else {
+            System.out.println("Ya existe un usuario con ese id");
+        }
+
+
+
     }
 }
